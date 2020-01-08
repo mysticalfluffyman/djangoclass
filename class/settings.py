d9488b6e8b9eedb216a63dac4210c5f97eeb4d38
mysticalfluffyman <rajbhandaripratik8@gmail.com>
2020-01-08 07:09:49 +0545

    newsapp1st

--- class/settings.py
+++ class/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # Custom App
+    'news.apps.NewsConfig',
 ]
 
 MIDDLEWARE = [
@@ -119,9 +121,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = "/static/"
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
     
 ]
+
+MEDIA_URL = "/media/"
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+
